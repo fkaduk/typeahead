1158a98e368f3ac7463536e649c4f76e376825a5
fkaduk <fkaduk@proton.me>
2026-01-30 05:10:28 +0100

    more screenshots

--- tests/testthat/test-screenshots.R
+++ tests/testthat/test-screenshots.R
@@ -58,4 +58,52 @@ describe("typeaheadInput screenshot tests", {
     app$expect_screenshot()
     app$stop()
   })
+
+  it("renders alongside other elements", {
+    app <- new_app(
+      shinyApp(
+        ui = fluidPage(
+          h3("Form"),
+          textInput("name", "Name"),
+          typeaheadInput(
+            inputId = "test",
+            label = "City",
+            choices = c("Berlin", "Boston", "Barcelona", "Brussels"),
+            placeholder = "Start typing..."
+          ),
+          selectInput("country", "Country", choices = c("DE", "US", "ES", "BE"))
+        ),
+        server = function(...) {}
+      ),
+      name = "typeahead-with-elements"
+    )
+
+    app$run_js(js_input_event_set("test", "B"))
+    app$wait_for_idle()
+    app$expect_screenshot()
+    app$stop()
+  })
+
+  it("renders with bslib Bootstrap 5 theme", {
+    app <- new_app(
+      shinyApp(
+        ui = bslib::page_fluid(
+          theme = bslib::bs_theme(version = 5),
+          typeaheadInput(
+            inputId = "test",
+            label = "BS5 Input",
+            choices = c("Apple", "Apricot", "Banana", "Cherry"),
+            placeholder = "Start typing..."
+          )
+        ),
+        server = function(...) {}
+      ),
+      name = "typeahead-bslib"
+    )
+
+    app$run_js(js_input_event_set("test", "A"))
+    app$wait_for_idle()
+    app$expect_screenshot()
+    app$stop()
+  })
 })
