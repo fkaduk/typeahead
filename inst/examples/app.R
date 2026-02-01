41edb7b52f162624f1b317ee60577fd02dbfbe35
fkaduk <fkaduk@proton.me>
2026-02-01 14:45:17 +0100

    add city button to demo

--- inst/examples/app.R
+++ inst/examples/app.R
@@ -56,7 +56,8 @@ ui <- page_sidebar(
   title = "typeahead demo",
   theme = bs_theme(version = 5, bootswatch = "flatly"),
   sidebar = sidebar(
-    actionButton("update", "Switch to fruits")
+    actionButton("update", "Switch to fruits"),
+    actionButton("update_cities", "Switch to cities")
   ),
   card(
     card_header("typeahead input"),
@@ -76,6 +77,14 @@ ui <- page_sidebar(
 server <- function(input, output, session) {
   output$selected <- renderText({
     input$city
+  })
+
+  observeEvent(input$update_cities, {
+    updateTypeaheadInput(
+      session = session,
+      inputId = "city",
+      choices = cities
+    )
   })
 
   observeEvent(input$update, {
